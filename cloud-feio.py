1b62fcd89f2b27917046a7b4c52de569fa7edb90
Leandro Oliveira <leosol@gmail.com>
2021-06-22 21:21:07 -0400

    options

--- cloud-feio.py
+++ cloud-feio.py
@@ -4,40 +4,105 @@ from StringIO import StringIO
 import ipaddress
 import time
 from datetime import date
+from datetime import datetime
 import signal
 import sys
 import argparse
 import os.path
 
-networkList = []
-hintStrings = []
 
 REQUESTED_SIGINT = 0
 SUCCESS_COUNT = 0
 SUCCESS_ITEMS = []
-def check_site(scheme, vhost, ipaddr, hintStrings, LOG_FILE):
+FOLLOW_LOCATION = 1
+TIMEOUT = 30
+URL_RESOURCE = '/'
+WORKERS = 250
+
+WORKDIR =  datetime.now().strftime("SCAN %Y-%m-%d at %H-%M-%S")
+
+def request_page_http(vhost, ipaddr):
+	global FOLLOW_LOCATION
+	global TIMEOUT
+	global URL_RESOURCE
+	buffer = StringIO()
+	curl = pycurl.Curl()
+	curl.setopt(curl.URL, 'http://'+vhost+URL_RESOURCE)
+	curl.setopt(curl.VERBOSE, 0)
+	curl.setopt(curl.CONNECTTIMEOUT, TIMEOUT)
+	curl.setopt(curl.TIMEOUT, TIMEOUT)
+	curl.setopt(curl.FOLLOWLOCATION, FOLLOW_LOCATION)
+	curl.setopt(curl.AUTOREFERER,1)
+	curl.setopt(curl.USERAGENT, 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36')
+	curl.setopt(curl.RESOLVE, [vhost+':80:'+ipaddr, vhost+':443:'+ipaddr])
+	curl.setopt(curl.WRITEDATA, buffer)
+	curl.perform()
+	status_code = curl.getinfo(curl.HTTP_CODE)
+	curl.close()
+	return {'status_code': status_code, 'body': buffer.getvalue()}
+
+def request_page_https_sni(vhost, ipaddr):
+	global FOLLOW_LOCATION
+	global TIMEOUT
+	global URL_RESOURCE
+	buffer = StringIO()
+	curl = pycurl.Curl()
+	curl.setopt(curl.URL, 'https://'+vhost+URL_RESOURCE)
+	curl.setopt(curl.VERBOSE, 0)
+	curl.setopt(curl.SSL_VERIFYPEER, False)
+	curl.setopt(curl.SSL_VERIFYHOST, False)
+	curl.setopt(curl.CONNECTTIMEOUT, TIMEOUT)
+	curl.setopt(curl.TIMEOUT, TIMEOUT)
+	curl.setopt(curl.FOLLOWLOCATION, FOLLOW_LOCATION)
+	curl.setopt(curl.AUTOREFERER,1)
+	curl.setopt(curl.USERAGENT, 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36')
+	curl.setopt(curl.RESOLVE, [vhost+':80:'+ipaddr, vhost+':443:'+ipaddr])
+	curl.setopt(curl.WRITEDATA, buffer)
+	curl.perform()
+	status_code = curl.getinfo(curl.HTTP_CODE)
+	curl.close()
+	return {'status_code': status_code, 'body': buffer.getvalue()}
+
+def request_page_https_nosni(vhost, ipaddr):
+	global FOLLOW_LOCATION
+	global TIMEOUT
+	global URL_RESOURCE
+	buffer = StringIO()
+	curl = pycurl.Curl()
+	curl.setopt(curl.HTTPHEADER, ['Host: '+vhost])
+	curl.setopt(curl.URL, 'https://'+ipaddr+URL_RESOURCE)
+	curl.setopt(curl.SSL_VERIFYPEER, False)
+	curl.setopt(curl.SSL_VERIFYHOST, False)
+	curl.setopt(curl.VERBOSE, 0)
+	curl.setopt(curl.CONNECTTIMEOUT, TIMEOUT)
+	curl.setopt(curl.TIMEOUT, TIMEOUT)
+	curl.setopt(curl.FOLLOWLOCATION, FOLLOW_LOCATION)
+	curl.setopt(curl.AUTOREFERER,1)
+	curl.setopt(curl.USERAGENT, 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36')
+	curl.setopt(curl.RESOLVE, [vhost+':80:'+ipaddr, vhost+':443:'+ipaddr])
+	curl.setopt(curl.WRITEDATA, buffer)
+	curl.perform()
+	status_code = curl.getinfo(curl.HTTP_CODE)
+	curl.close()
+	return {'status_code': status_code, 'body': buffer.getvalue()}	
+
+def check_site(scheme, vhost, ipaddr, hintStrings, LOG_FILE): 
 	global SUCCESS_COUNT
 	global SUCCESS_ITEMS
-	#print('check_site:\t'+ipaddr+'('+scheme+')')
 	sys.stdout.write('.')
 	sys.stdout.flush()
 	try:
-		buffer = StringIO()
-		curl = pycurl.Curl()
-		curl.setopt(curl.URL, scheme+'://'+vhost+'/')
-		curl.setopt(curl.VERBOSE, 0)
-		curl.setopt(curl.CONNECTTIMEOUT, 30)
-		curl.setopt(curl.TIMEOUT, 30)
-		curl.setopt(curl.FOLLOWLOCATION, 1)
-		curl.setopt(curl.AUTOREFERER,1)
-		curl.setopt(curl.USERAGENT, 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36')
-		curl.setopt(curl.RESOLVE, [vhost+':80:'+ipaddr, vhost+':443:'+ipaddr])
-		curl.setopt(curl.WRITEDATA, buffer)
-		curl.perform()
-		status_code = curl.getinfo(curl.HTTP_CODE)
-		curl.close()
-		
-		body = buffer.getvalue()
+		response = {}
+		body = ''
+		if scheme == 'http':
+			response = request_page_http(vhost, ipaddr)
+		elif scheme == 'https':
+			response = request_page_https_nosni(vhost, ipaddr)
+		elif scheme == 'https (SNI)':
+			response = request_page_https_sni(vhost, ipaddr)
+
+		body = response['body']
+		status_code = response['status_code']
 		score = 0
 		for s in hintStrings:
 			if s in body:
@@ -45,7 +110,7 @@ def check_site(scheme, vhost, ipaddr, hintStrings, LOG_FILE):
 		foundStr = ""
 		if score>0:
 			foundStr = " (FOUND!) "
-			respfile = open('resp_'+scheme+'-'+ipaddr+'.dat', 'w')
+			respfile = open('./'+WORKDIR+'/resp_'+scheme+'-'+ipaddr+'.dat', 'w')
 			respfile.write(body)
 			respfile.flush()
 			respfile.close()
@@ -57,68 +122,18 @@ def check_site(scheme, vhost, ipaddr, hintStrings, LOG_FILE):
 		LOG_FILE.write("Scheme:\t"+scheme+'\n')
 		LOG_FILE.write("Code:\t"+ str(status_code)+'\n')
 		LOG_FILE.write("Score:\t"+str(score)+foundStr+'\n')
+		LOG_FILE.write("_________________________\n")
 	except:
 		LOG_FILE.write("IP:\t"+ipaddr+" (failed)\n")
 		LOG_FILE.write("Scheme:\t"+scheme+'\n')
 		LOG_FILE.write("Code:\t-1\n")
 		LOG_FILE.write("Score:\t-1\n")	
-	LOG_FILE.flush()	
-
-def check_site2(scheme, vhost, ipaddr, hintStrings, LOG_FILE): 
-	global SUCCESS_COUNT
-	global SUCCESS_ITEMS
-	sys.stdout.write('.')
-	sys.stdout.flush()
-	try:
-		buffer = StringIO()
-		curl = pycurl.Curl()
-		curl.setopt(curl.HTTPHEADER, ['HOST: '+vhost])
-		curl.setopt(curl.URL, scheme+'://'+ipaddr+'/')
-		curl.setopt(curl.SSL_VERIFYPEER, False)
-		curl.setopt(curl.SSL_VERIFYHOST, False)
-		curl.setopt(curl.VERBOSE, 0)
-		curl.setopt(curl.CONNECTTIMEOUT, 30)
-		curl.setopt(curl.TIMEOUT, 30)
-		curl.setopt(curl.FOLLOWLOCATION, 1)
-		curl.setopt(curl.AUTOREFERER,1)
-		curl.setopt(curl.USERAGENT, 'Mozilla/5.0 AppleWebKit/537.36 (KHTML, like Gecko; compatible; Googlebot/2.1; +http://www.google.com/bot.html) Safari/537.36')
-		curl.setopt(curl.RESOLVE, [vhost+':80:'+ipaddr, vhost+':443:'+ipaddr])
-		curl.setopt(curl.WRITEDATA, buffer)
-		curl.perform()
-		status_code = curl.getinfo(curl.HTTP_CODE)
-		curl.close()
-		
-		body = buffer.getvalue()
-		score = 0
-		for s in hintStrings:
-			if s in body:
-				score = score + 1
-		foundStr = ""
-		if score>0:
-			foundStr = " (FOUND!) "
-			respfile = open('resp_'+scheme+'-'+ipaddr+'.dat', 'w')
-			respfile.write(body)
-			respfile.flush()
-			respfile.close()
-			SUCCESS_COUNT = SUCCESS_COUNT+1
-			SUCCESS_ITEMS.append('resp_'+scheme+'-'+ipaddr+'.dat')
-			
-
-		LOG_FILE.write("IP:\t"+ipaddr+'\n')
-		LOG_FILE.write("Scheme:\t"+scheme+'\n')
-		LOG_FILE.write("Code:\t"+ str(status_code)+'\n')
-		LOG_FILE.write("Score:\t"+str(score)+foundStr+'\n')
-	except:
-		LOG_FILE.write("IP:\t"+ipaddr+" (failed)\n")
-		LOG_FILE.write("Scheme:\t"+scheme+'\n')
-		LOG_FILE.write("Code:\t-1\n")
-		LOG_FILE.write("Score:\t-1\n")	
+		LOG_FILE.write("_________________________\n")
 	LOG_FILE.flush()
 
 def spawnNetworks(networkList):
 	result = []
         for networkItem in networkList:
-		#print("Spawning network "+networkItem)
 		network = ipaddress.ip_network(unicode(networkItem))
             	for item in network.hosts():
                 	result.append(item)
@@ -133,6 +148,7 @@ def findVhost(ipList, domain, LOG_FILE):
 		start_time = time.time()
 		check_site('http', domain, str(ip), LOG_FILE)
 		check_site('https', domain, str(ip), LOG_FILE)
+		check_site('https', domain, str(ip), LOG_FILE)
 		spent_time = (time.time() - start_time)
 		execution_time = execution_time+spent_time
 		processedItems = processedItems+1
@@ -142,8 +158,9 @@ def findVhost(ipList, domain, LOG_FILE):
 			print("Everage:\t"+str(execution_time/processedItems))
 
 def doFindVhostWithThreadPool(ipaddr, hostname, hintStrings, LOG_FILE):
-	#check_site2('http', hostname, str(ipaddr), hintStrings, LOG_FILE)
-	check_site2('https', hostname, str(ipaddr), hintStrings, LOG_FILE)
+	check_site('http', hostname, str(ipaddr), hintStrings, LOG_FILE)
+	check_site('https', hostname, str(ipaddr), hintStrings, LOG_FILE)
+	check_site('https (SNI)', hostname, str(ipaddr), hintStrings, LOG_FILE)
 
 processedItems = 0
 def future_callback_error_logger(future):
@@ -159,11 +176,12 @@ def findVhostWithThreadPool(ipList, hostname, hintStrings, LOG_FILE):
 	global processedItems
 	global SUCCESS_COUNT
 	global SUCCESS_ITEMS
+	global WORKERS
 	LOG_FILE.write('Starting FIND-VHOST AT '+str(date.today())+'\n')
 	totalitems = len(ipList)
 	LOG_FILE.write('Total hosts '+str(totalitems)+'\n')
 	start_time = time.time()
-	executor = concurrent.futures.ThreadPoolExecutor(max_workers=250)
+	executor = concurrent.futures.ThreadPoolExecutor(max_workers=WORKERS)
 	with executor:
 		futures = []
 		for item in ipList:
@@ -195,26 +213,58 @@ def launchChecker(domain, networks, texts, LOG_FILE):
 	else:
 		print('Okay, maybe later...')
 
-parser = argparse.ArgumentParser(description='Find Vhost')
-parser.add_argument('domain', help='Domain name (www.example.com).')
-parser.add_argument('networks', help='A file with a list of networks or a single network (192.168.1.0/24)')
-parser.add_argument('expected-txt', help='Texts that actually exists in the expected response.')
+#parser.add_argument('chars', type=str, nargs=2, metavar='c',help='starting and ending character')
+parser = argparse.ArgumentParser(description='Find Vhost in many lans')
+parser.add_argument('domain', type=str, help='Domain name (www.example.com).')
+parser.add_argument('networks', type=str, help='A file with a list of networks or a single network (192.168.1.0/24)')
+parser.add_argument('expected-txt', type=str, help='Texts that actually exists in the expected response.')
+parser.add_argument('follow-redir', type=int, nargs='?', help='{0,1} Follow redirects (defaults to 1 - yes)')
+parser.add_argument('timeout', type=int, nargs='?', help='Timeout (defaults to 30s)')
+parser.add_argument('url-resource',type=str, nargs='?', help='Extra part of the URL - defaults to / (slash)')
+parser.add_argument('workers', type=int, nargs='?', help='Max open requests at a single time (defaults to 250)')
+
 args = vars(parser.parse_args())
 domain = args["domain"]
 networks = args["networks"]
 expected_txt = args["expected-txt"]
+follow_redir = args['follow-redir']
+timeout = args['timeout']
+url_resource = args['url-resource']
+workers = args['workers']
 
 if domain is None:
 	print('Domain not set')
 	exit()
-
 if networks is None:
 	print('networks not set')
 	exit()
-
 if expected_txt is None:
 	print('expected_txt not set')
 	exit()
+if follow_redir is None:
+	print('Follow location enabled (default option)')
+	FOLLOW_LOCATION = 1
+else:
+	FOLLOW_LOCATION = int(follow_redir)
+if timeout is None:
+	TIMEOUT = 30
+	print('Using default timeout of 30s')
+else:
+	TIMEOUT = int(timeout)
+if url_resource is None:
+	URL_RESOURCE = '/'
+	print('No URL Resource was set')
+else:
+	if not url_resource.startswith('/'):
+		print('URL RESOURCE SHOULD START WITH /')
+		exit()
+	else:
+		URL_RESOURCE = url_resource
+if workers is None:
+	WORKERS = 250
+	print('Using 250 as the number of Workers')
+else:
+	WORKERS = int(workers)
 
 var_networks = []
 if os.path.isfile(networks):
@@ -232,7 +282,15 @@ if os.path.isfile(expected_txt):
 else:
 	var_expected_txt.append(expected_txt)
 
-LOG_FILE = open('find-vhost.log', 'w')
+
+if not os.path.exists(os.path.dirname('./'+WORKDIR+'/find-vhost.log')):
+    try:
+        os.makedirs(os.path.dirname('./'+WORKDIR+'/find-vhost.log'))
+    except OSError as exc: 
+        if exc.errno != errno.EEXIST:
+            raise
+
+LOG_FILE = open('./'+WORKDIR+'/find-vhost.log', 'w')
 launchChecker(domain, var_networks, var_expected_txt, LOG_FILE)
 LOG_FILE.close()
 
